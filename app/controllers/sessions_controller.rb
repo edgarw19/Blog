015a732adfe1c52e8c57b6e36ecf6e8f4191690e
edgarw19 <edgarallenpoed@gmail.com>
2014-02-02 12:05:12 -0500

    minor fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:session][:email])
-  if user && user.authenticate(params[:session][:password])
+  	user = User.authenticate(params[:email], params[:password])
+  if user
     session[:user_id] = user.id
     sign_in user
     redirect_to root_url, :notice => "Logged in!"
