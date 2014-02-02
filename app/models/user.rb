60a90b1f460d0a9fe27bd51d9693116118826a0e
edgarw19 <edgarallenpoed@gmail.com>
2014-02-02 18:14:14 -0500

    user changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,17 +28,9 @@ class User < ActiveRecord::Base
     end
   end
 
-  def User.new_remember_token
-    SecureRandom.urlsafe_base64
-  end
-
-  def User.encrypt(token)
-    Digest::SHA1.hexdigest(token.to_s)
-  end
-
   private
 
     def create_remember_token
-      self.remember_token = User.encrypt(User.new_remember_token)
+      self.remember_token = SecureRandom.urlsafe_base64
     end
 end
